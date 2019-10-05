3de7dc7fa9c77fad2d6ae1f4c25434c5ed7e781c
Takamichi Omori <merrytakamerry@gmail.com>
2019-10-06 04:43:16 +0900

    iikanji

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 
 const port = 5000;
+const APP1 = 'https://nowdir.falconfusa.now.sh/'
+const APP2 = 'https://backlog-search-service.ripicea.now.sh/'
 
 // Body parser
 app.use(bodyParser());
@@ -22,19 +24,22 @@ app.post("/webhook", (req, res) => {
   const requestMessage = req.body.post.message;
   const requestId = req.body.post.id;
   const message = requestMessage.replace("　", " ").split(' ');
-  if(message[0] === '課題くれ'){
-    // APP2
-    res.send({
-      "message": "あげる",
-      "replyTo":requestId
-    })
-  }else{
-    // APP1
-    res.send({
-      "message": "こんにちは",
-      "replyTo":requestId
-    })
-  }
-  
-  
+
+  const sw = (message[0] === '課題くれ')
+
+  axios.post(sw ? APP2 : APP1, {"request": sw ? ' ': message[0]} ).then(
+    response =>{
+      console.log(response.data.response)
+      if(response.data.response.length !== 0){
+        res.send({
+          "message": response.data.response.join(' '),
+          "replyTo":requestId
+        })
+      }else{
+        res.end()
+      }
+    }
+  ).catch(
+    err => console.error(err)
+  )
 });
